85eb724a45b754d21a7f6edaef1c46609b877d30
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-07-16 10:26:28 +0100

    Added run-version.sh for macOS.

---
+++ MacOS_x86/run-version2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# regular case, no replacement:
+echo 1234567890*bcdefghijklmnopqrst1234567890ABCDEFGHIJKLMNOPQRST1234567890abcdefghij_ | ./squasher2.exe
+exit_code=$?
+[ $exit_code -ne 1 ] && echo "\nFAILED with exit code: $exit_code"
+echo ""
+echo ""
+
+# special case, with replacement:
+echo 1234567890**cdefghijklmnopqrst1234567890ABCDEFGHIJKLMNOPQRST1234567890abcdefghij_ | ./squasher2.exe
+exit_code=$?
+[ $exit_code -ne 1 ] && echo "\nFAILED with exit code: $exit_code"
+echo ""
+echo ""
